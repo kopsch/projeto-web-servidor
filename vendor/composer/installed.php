e5b5e6eb4fff8ac0c08c1ca5d0c1bd7a46a774b7
Pedro Kopsch <pedro.kopsch@prestes.com>
2023-04-20 10:19:00 -0300

    vendor added

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '580edf6f5bd52d1e75981848ac94893736c67614',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '580edf6f5bd52d1e75981848ac94893736c67614',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
