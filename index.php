e7f3d8237f0a38bb088b56d479f3af9fb2e5007c
Pedro Kopsch <pedro.kopsch@prestes.com>
2023-04-20 11:42:47 -0300

    fix: index.php atualizado

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
 
-define('BASE_URL', '/projeto-web-servidor/');
+define('BASE_URL', $_SERVER['REQUEST_URI']);
 session_start();
 
 $method = $_SERVER['REQUEST_METHOD'];
