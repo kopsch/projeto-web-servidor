6a58dbb94ec647bdd19e530e4702941a71d02319
Pedro Kopsch <pedro.kopsch@prestes.com>
2023-04-20 09:42:24 -0300

    style: identacao

--- index.php
+++ index.php
@@ -13,46 +13,46 @@ define('BASE_URL', '/projeto-web-servidor/');
 session_start();
 
 $method = $_SERVER['REQUEST_METHOD'];
-    $path = $_SERVER['PATH_INFO'] ?? '/';
-    
-    $userService = new UserService();
-    $userController = new UserController($userService);
-    
-    if ($_SERVER['REQUEST_URI'] == BASE_URL) {
-        if(!isset($_SESSION['user'])) {
-            require_once 'app/views/login.php';
-        } else {
-            header('Location: /projeto-web-servidor/home');
-        }
-    } elseif ($_SERVER['REQUEST_URI'] == BASE_URL . 'home') {
-        if(!isset($_SESSION['user'])) {
-            header('Location: /projeto-web-servidor');
-        } else {
-            require_once 'app/views/session.php';
-        }
-    } elseif ($_SERVER['REQUEST_URI'] == BASE_URL . 'register') {
-        if(!isset($_SESSION['user'])) {
-            require_once 'app/views/register.php';
-        } else {
-            header('Location: /projeto-web-servidor/home');
-        }
-    } elseif ($_SERVER['REQUEST_URI'] == BASE_URL . 'edit') {
-        if(!isset($_SESSION['user'])) {
-            header('Location: /projeto-web-servidor');
-        } else {
-            require_once 'app/views/edit.php';
-        }
-    } elseif ($_SERVER['REQUEST_URI'] == BASE_URL . 'api/login' && $_SERVER['REQUEST_METHOD'] == 'POST') {
-        $userController->login();
-    } elseif ($_SERVER['REQUEST_URI'] == BASE_URL . 'api/logout' && $_SERVER['REQUEST_METHOD'] == 'GET') {
-        $userController->logout();
-    } elseif ($_SERVER['REQUEST_URI'] == BASE_URL . 'api/register' && $_SERVER['REQUEST_METHOD'] == 'POST') {
-        $userController->register();
-    } elseif ($_SERVER['REQUEST_URI'] == BASE_URL . 'api/edit' && $_SERVER['REQUEST_METHOD'] == 'POST') {
-        $userController->edit();
+$path = $_SERVER['PATH_INFO'] ?? '/';
+
+$userService = new UserService();
+$userController = new UserController($userService);
+
+if ($_SERVER['REQUEST_URI'] === BASE_URL) {
+    if (!isset($_SESSION['user'])) {
+        require_once 'app/views/login.php';
     } else {
-        header('HTTP/1.0 404 Not Found');
-        echo 'Página não encontrada';
+        header('Location: /projeto-web-servidor/home');
     }
+} elseif ($_SERVER['REQUEST_URI'] === BASE_URL . 'home') {
+    if (!isset($_SESSION['user'])) {
+        header('Location: /projeto-web-servidor');
+    } else {
+        require_once 'app/views/session.php';
+    }
+} elseif ($_SERVER['REQUEST_URI'] === BASE_URL . 'register') {
+    if (!isset($_SESSION['user'])) {
+        require_once 'app/views/register.php';
+    } else {
+        header('Location: /projeto-web-servidor/home');
+    }
+} elseif ($_SERVER['REQUEST_URI'] === BASE_URL . 'edit') {
+    if (!isset($_SESSION['user'])) {
+        header('Location: /projeto-web-servidor');
+    } else {
+        require_once 'app/views/edit.php';
+    }
+} elseif ($_SERVER['REQUEST_URI'] === BASE_URL . 'api/login' && $_SERVER['REQUEST_METHOD'] === 'POST') {
+    $userController->login();
+} elseif ($_SERVER['REQUEST_URI'] === BASE_URL . 'api/logout' && $_SERVER['REQUEST_METHOD'] === 'GET') {
+    $userController->logout();
+} elseif ($_SERVER['REQUEST_URI'] === BASE_URL . 'api/register' && $_SERVER['REQUEST_METHOD'] === 'POST') {
+    $userController->register();
+} elseif ($_SERVER['REQUEST_URI'] === BASE_URL . 'api/edit' && $_SERVER['REQUEST_METHOD'] === 'POST') {
+    $userController->edit();
+} else {
+    header('HTTP/1.0 404 Not Found');
+    echo 'Página não encontrada';
+}
 
 ?>
